f0d30916a0a32e12bb244fa115bfb9b420b762d5
Justin Lange <well1912@gmail.com>
2017-03-08 00:33:38 -0800

    get SECRET_KEY from environment

--- recipes/settings.py
+++ recipes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tr_p$yzxby-lk-$$*%$k4s1glw6rdch!5@hg9%ez&i%pzrm5)u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') != 'False'
