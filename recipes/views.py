058b5b8a106fa9f1fb2c2b5e17606acb599afd7b
Justin Lange <well1912@gmail.com>
2017-03-15 02:11:19 -0700

    pep8 newlines

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.base import TemplateView
 
+
 class HomePage(TemplateView):
     """View for the home page."""
     template_name = 'recipes/home.html'
+
 
 class AboutPage(TemplateView):
     """View for the about page."""
