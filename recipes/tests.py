8d6140a749bdd9477b3dcae95edede5352df4d39
Justin Lange <well1912@gmail.com>
2017-03-19 15:42:31 -0700

    Have home page show most recent recipes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,15 +1,39 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from recipe.models import Recipe
 
 
-class StaticPageTests(TestCase):
-    """Tests for static pages."""
+class HomePageTests(TestCase):
+    """Tests for home page."""
 
     def setUp(self):
         """Initialize a response to be tested."""
-        self.home = self.client.get(reverse('home'))
-        self.about = self.client.get(reverse('about'))
+        user = User(username='testuser')
+        user.save()
+        for i in range(10):
+            Recipe(
+                user=user,
+                title='title {}'.format(i),
+                description='desc',
+                ingredients='ing',
+                directions='dir'
+            ).save()
+        self.response = self.client.get(reverse('home'))
 
     def testHomePageHasSearch(self):
         """Home page has search form."""
-        self.assertContains(self.home, 'input')
+        self.assertContains(self.response, 'input')
+
+    def testHomePageHasRecipes(self):
+        """Home page has most recent recipes."""
+        for recipe in Recipe.objects.reverse()[:5]:
+            self.assertContains(self.response, recipe.title)
+
+
+class AboutPageTests(TestCase):
+    """Tests for about page."""
+
+    def setUp(self):
+        """Initialize a response to be tested."""
+        self.response = self.client.get(reverse('about'))
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.views.generic.base import TemplateView
+from recipe.models import Recipe
 
 
 class HomePage(TemplateView):
     """View for the home page."""
     template_name = 'recipes/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePage, self).get_context_data(**kwargs)
+        context['recipes'] = Recipe.objects.all()[:5]
+        return context
 
 
 class AboutPage(TemplateView):
