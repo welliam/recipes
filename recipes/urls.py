635019a09925489fc78ce2f1ea31f1ee596b9614
Justin Lange <well1912@gmail.com>
2017-03-28 20:59:39 -0700

    Use 'accounts' instead of 'account' as registration urls prefix

--- recipes/urls.py
+++ recipes/urls.py
@@ -19,7 +19,7 @@ from .views import HomePage, AboutPage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^account/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^$', HomePage.as_view(), name='home'),
     url(r'^about/', AboutPage.as_view(), name='about'),
     url(r'^profile/', include('user_profile.urls')),
