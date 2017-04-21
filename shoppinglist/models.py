617689cb94c9624167febd0b0469a9c31be11f1c
Justin Lange <well1912@gmail.com>
2017-04-20 23:38:58 -0700

    Give ShoppingLists random ids

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -1,9 +1,24 @@
+from random import randint
 from django.db import models
 from django.contrib.auth.models import User
 
 
+def create_unique_urlindex(lower_bound=100000000):
+    """Creates a unique urlindex.
+
+    Ensures the generated value is not already a urlindex."""
+    while True:
+        i = randint(lower_bound, lower_bound*10-1)
+        if not ShoppingList.objects.filter(id=i).exists():
+            return i
+
+
 class ShoppingList(models.Model):
     """Model for user shopping lists."""
+    id = models.PositiveIntegerField(
+        primary_key=True,
+        default=create_unique_urlindex
+    )
     user = models.ForeignKey(
         User,
         related_name='shoppinglists',
