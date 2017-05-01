79fd8683ccc72e0f3f191fff853b878933a07fd7
Justin Lange <well1912@gmail.com>
2017-04-30 21:55:02 -0700

    Show user's shopping list items in detail view

--- shoppinglist/tests.py
+++ shoppinglist/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .models import ShoppingListItem
 
 
 class ShoppingListDetailView(TestCase):
@@ -9,7 +10,17 @@ class ShoppingListDetailView(TestCase):
         self.user.save()
         self.client.force_login(self.user)
         url = reverse('view_shopping_list')
+        self.items = [
+            ShoppingListItem(user=self.user, title='item#{}'.format(i))
+            for i in range(50)
+        ]
+        for item in self.items:
+            item.save()
         self.response = self.client.get(url)
 
-    def test_shopping_response_has_titles(self):
+    def test_shopping_response_status_code(self):
         self.assertEqual(self.response.status_code, 200)
+
+    def test_shopping_response_has_titles(self):
+        for item in self.items:
+            self.assertContains(self.response, item.title)
